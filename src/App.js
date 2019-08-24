b9f4275aa3c16b3dbf78d13be8c818827d919a60
johnson chidubem <johnsonchidubem@johnsons-MacBook-Pro.local>
2019-08-24 22:23:04 +0100

    added team page with a submit form for newsletter

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import "./App.css";
 import Landing from "./components/pages/landing";
 import About from "./components/pages/about";
+import Team from "./components/pages/team";
 import Footer from "./components/pages/partials/footer";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Landing} />
         <Route path="/about" component={About} />
+        <Route path="/team" component={Team} />
       </Switch>
       <Footer />
     </Router>
--- src/components/pages/partials/navbar.jsx
+++ src/components/pages/partials/navbar.jsx
@@ -43,7 +43,7 @@ const Navbar = () => (
           </li>
 
           <li className="nav-item">
-            <Link to="" className="nav-link menu-mrg a-2">
+            <Link to="/team" className="nav-link menu-mrg a-2">
               Our team
             </Link>
           </li>
